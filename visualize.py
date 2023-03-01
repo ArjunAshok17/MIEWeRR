0eb17e7a31ad63d5dcce36d958a4656c41db4065
Arjun Ashok <arjun3.ashok@gmail.com>
2023-02-28 23:57:52 -0800

    Small changes to visualizer

--- visualize.py
+++ visualize.py
@@ -1,5 +1,9 @@
 # Program Description #
 # Defines the functions necessary to visualize all the regressive looks #
 
-import matplotlib as mpl
-import 
+import matplotlib as plt            # graphing
+from sklearn import linear_model    # graph regressive looks
+
+
+# plot dataset #
+def plot_data()
